5d1a592d43abf6896d96b8dc73924141510deb3d
Christof Petig <christof.petig@arcor.de>
2025-08-17 18:24:49 +0200

    more testing skeleton

--- examples/complex/starter/build-wasm.sh
+++ examples/complex/starter/build-wasm.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+(cd ../publisher; cargo build --target wasm32-wasip1 --no-default-features -F canonical)
+(cd ../consumer; cargo build --target wasm32-wasip1 --no-default-features -F canonical)
 cargo build --target wasm32-wasip1 --no-default-features -F canonical
