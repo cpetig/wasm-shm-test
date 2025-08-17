c132e5d77493f756547cfa4c2652a0d6e90f47e5
Christof Petig <christof.petig@arcor.de>
2025-08-17 21:48:33 +0200

    wasm build script

--- examples/complex/starter/build-wasm.sh
+++ examples/complex/starter/build-wasm.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 (cd ../publisher; cargo build --target wasm32-wasip1 --no-default-features -F canonical)
+(cd ../publisher ; wasm-tools component new --adapt ../../../wasi_snapshot_preview1.reactor.wasm target/wasm32-wasip1/debug/publisher.wasm -o component.wasm )
 (cd ../consumer; cargo build --target wasm32-wasip1 --no-default-features -F canonical)
+(cd ../consumer ; wasm-tools component new --adapt ../../../wasi_snapshot_preview1.reactor.wasm target/wasm32-wasip1/debug/consumer.wasm -o component.wasm )
 cargo build --target wasm32-wasip1 --no-default-features -F canonical
+wasm-tools component new --adapt ../../../wasi_snapshot_preview1.command.wasm target/wasm32-wasip1/debug/starter.wasm -o component.wasm 
+wasm-tools compose  -o combined1.wasm component.wasm -d publisher.wasm  --skip-validation 
+wasm-tools compose  -o combined2.wasm combined1.wasm -d consumer.wasm  --skip-validation 
+wasm-tools compose  -o combined3.wasm combined2.wasm -d ../../../symmetric/component.wasm  --skip-validation 
