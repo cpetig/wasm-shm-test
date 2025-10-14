8d59e0fae2eb952e1310ab696dc7ed222be95413
Christof Petig <christof.petig@arcor.de>
2025-10-15 00:48:53 +0200

    adapt to new location

--- examples/complex/starter/build-wasm.sh
+++ examples/complex/starter/build-wasm.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 (cd ../publisher; cargo build --target wasm32-wasip1 --no-default-features -F canonical)
-(cd ../publisher ; wasm-tools component new --adapt ../../../wasi_snapshot_preview1.reactor.wasm target/wasm32-wasip1/debug/publisher.wasm -o component.wasm )
+(cd ../publisher ; wasm-tools component new --adapt ../../../wasi_snapshot_preview1.reactor.wasm ../../target/wasm32-wasip1/debug/publisher.wasm -o component.wasm )
 (cd ../consumer; cargo build --target wasm32-wasip1 --no-default-features -F canonical)
-(cd ../consumer ; wasm-tools component new --adapt ../../../wasi_snapshot_preview1.reactor.wasm target/wasm32-wasip1/debug/consumer.wasm -o component.wasm )
+(cd ../consumer ; wasm-tools component new --adapt ../../../wasi_snapshot_preview1.reactor.wasm ../../target/wasm32-wasip1/debug/consumer.wasm -o component.wasm )
 cargo build --target wasm32-wasip1 --no-default-features -F canonical
-wasm-tools component new --adapt ../../../wasi_snapshot_preview1.command.wasm target/wasm32-wasip1/debug/starter.wasm -o component.wasm 
+wasm-tools component new --adapt ../../../wasi_snapshot_preview1.command.wasm ../../target/wasm32-wasip1/debug/starter.wasm -o component.wasm 
 wasm-tools compose  -o combined1.wasm component.wasm -d publisher.wasm  --skip-validation 
 wasm-tools compose  -o combined2.wasm combined1.wasm -d consumer.wasm  --skip-validation 
 wasm-tools compose  -o combined.wasm combined2.wasm -d ../../../pub-sub/component.wasm  --skip-validation 
--- examples/complex/starter/build-wasm2.sh
+++ examples/complex/starter/build-wasm2.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 (cd ../consumer; cargo build --target wasm32-wasip1 --no-default-features -F canonical,combined)
-(cd ../consumer ; wasm-tools component new --adapt ../../../wasi_snapshot_preview1.reactor.wasm target/wasm32-wasip1/debug/consumer.wasm -o component.wasm )
+(cd ../consumer ; wasm-tools component new --adapt ../../../wasi_snapshot_preview1.reactor.wasm ../../target/wasm32-wasip1/debug/consumer.wasm -o component.wasm )
 cargo build --target wasm32-wasip1 --no-default-features -F canonical
-wasm-tools component new --adapt ../../../wasi_snapshot_preview1.command.wasm target/wasm32-wasip1/debug/starter.wasm -o component.wasm 
+wasm-tools component new --adapt ../../../wasi_snapshot_preview1.command.wasm ../../target/wasm32-wasip1/debug/starter.wasm -o component.wasm 
 #wasm-tools compose  -o combined1.wasm component.wasm -d publisher.wasm  --skip-validation 
 wasm-tools compose  -o combined2.wasm component.wasm -d consumer.wasm  --skip-validation 
 wasm-tools compose  -o combined.wasm combined2.wasm -d ../../../pub-sub/component.wasm  --skip-validation 
