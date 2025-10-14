9532f26766a3c49c20c760fda73d075168e55da4
Christof Petig <christof.petig@arcor.de>
2025-10-15 00:32:06 +0200

    update wit-bindgen with fix

--- runtime/build.sh
+++ runtime/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-(cd ../impl; cargo build --target wasm32-wasip2)
-(cd ../main; cargo build --target wasm32-wasip2)
+(cd ../examples/minimal/impl; cargo build --target wasm32-wasip2)
+(cd ../examples/minimal/main; cargo build --target wasm32-wasip2)
 wasm-tools compose -o combined.wasm main.wasm -d implementation.wasm
